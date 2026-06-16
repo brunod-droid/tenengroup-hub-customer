47f66bac5a9670a3aa1a374ba919eb51831adccb
brunod-droid <bruno.d@tenengroup.com>
2026-06-16 08:49:51 +0300

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,31 +1,17 @@
 import { NextResponse } from 'next/server';
 
 export function middleware(request) {
-  const auth = request.headers.get('authorization');
-  const username = process.env.SITE_USERNAME;
-  const password = process.env.SITE_PASSWORD;
-
-  if (!username || !password) {
-    return new Response('Site password is not configured.', { status: 500 });
-  }
-
-  if (auth) {
-    const token = auth.split(' ')[1] || '';
-    const [user, pass] = Buffer.from(token, 'base64').toString().split(':');
-
-    if (user === username && pass === password) {
-      return NextResponse.next();
+  return new Response(
+    JSON.stringify({
+      username: process.env.SITE_USERNAME || "MISSING",
+      password: process.env.SITE_PASSWORD ? "FOUND" : "MISSING",
+    }),
+    {
+      headers: { "content-type": "application/json" },
     }
-  }
-
-  return new Response('Authentication required', {
-    status: 401,
-    headers: {
-      'WWW-Authenticate': 'Basic realm="Protected Site"',
-    },
-  });
+  );
 }
 
 export const config = {
-  matcher: ['/((?!_next/static|_next/image|favicon.ico|robots.txt).*)'],
+  matcher: "/:path*",
 };
